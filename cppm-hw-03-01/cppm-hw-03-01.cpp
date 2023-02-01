370c2aca964f03fc66ba5e307446c1cbe772f9ac
Eugene Zemlyanko <slayer0007@mail.ru>
2023-02-01 21:32:38 +0300

    Add solution #3

---
+++ cppm-hw-03-01/cppm-hw-03-01.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <string>
+
+class Calculate
+{
+public:
+    double num1;
+    double num2;
+
+    double add() {return num1 + num2;}
+
+    double multiply() {return num1 * num2;}
+
+    double substruct_1_2() {return num1 - num2;}
+
+    double substruct_2_1() {return num2 - num1;}
+
+    double divide_1_2() {return num1 / num2;}
+
+    double divide_2_1() {return num2 / num1;}
+
+    bool set_num1(double num1)
+    {
+        if (num1)
+        {
+            this->num1 = num1;
+            return true;
+        }
+        else return false;
+    }
+
+    bool set_num2(double num2)
+    {
+        if (num2)
+        {
+            this->num2 = num2;
+            return true;
+        }
+        else return false;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "ru-RU");
+    system("chcp 1251");
+
+    float num1, num2;
+    Calculate calculate;
+
+    do
+    {
+        do
+        {
+            std::cout << "Введите num1: ";
+            std::cin >> num1;
+            if (!calculate.set_num1(num1))
+            {
+                std::cout << "Неверный ввод!" << std::endl;
+            }
+        } while (!calculate.set_num1(num1));
+
+        do
+        {
+            std::cout << "Введите num2: ";
+            std::cin >> num2;
+            if (!calculate.set_num2(num2))
+            {
+                std::cout << "Неверный ввод!" << std::endl;
+            }
+        } while (!calculate.set_num2(num2));
+
+        std::cout << "num1 + num2 = " << calculate.add() << std::endl;
+        std::cout << "num1 - num2 = " << calculate.substruct_1_2() << std::endl;
+        std::cout << "num2 - num1 = " << calculate.substruct_2_1() << std::endl;
+        std::cout << "num1 * num2 = " << calculate.multiply() << std::endl;
+        std::cout << "num1 / num2 = " << calculate.divide_1_2() << std::endl;
+        std::cout << "num2 / num1 = " << calculate.divide_2_1() << std::endl;
+
+    } while (true);
+}
---
+++ cppm-hw-03-02/cppm-hw-03-02.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <string>
+
+class Counter
+{
+    public:
+        Counter(int counter = 1) { this->counter = counter; }
+    
+ //       Counter() { counter = 1; }
+    
+        void increment() { ++counter; }
+    
+        void decrement() { --counter; }
+
+        int getItem() { return counter; }
+    
+    private:
+        int counter;
+};
+
+int main()
+{
+    setlocale(LC_ALL, "ru-RU");
+    system("chcp 1251");
+    
+
+    Counter *p_Counter = nullptr;
+
+    std::string firstItemId;
+    int firstItem;
+
+    std::cout << "Вы хотите указать начальное значение счетчика? Ведите да или нет: ";
+    std::cin >> firstItemId;
+
+    if (firstItemId == "да")
+    {
+        std::cout << "Введите начальное значение счетчика: ";
+        std::cin >> firstItem;
+        p_Counter = new Counter(firstItem);
+
+    }
+    else
+    {
+        p_Counter = new Counter;
+    }
+
+    char operation;
+
+    do
+    {
+        std::cout << "Введите команду ('+', '-', '=' или 'x'):";
+        std::cin >> operation;
+        if (operation == '+') { p_Counter->increment(); }
+        if (operation == '-') { p_Counter->decrement(); }
+        if (operation == '=')
+        {
+            std::cout << p_Counter->getItem() << std::endl;
+        }
+    } while (operation != 'х');
+    std::cout << "До свидания";
+
+}
