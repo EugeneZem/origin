7504ec4268ed52c3063a6204e05c5b6414be92a6
Eugene Zemlyanko <slayer0007@mail.ru>
2023-02-03 23:29:46 +0300

    Add solution #4

---
+++ cppm-hw-04-01/cppm-hw-04-01.cpp
@@ -0,0 +1,96 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+class Base
+{
+public:
+	class Adress
+	{
+	public:
+		std::string city;
+		std::string street;
+		int house;
+		int flat;
+
+		Adress(std::string city, std::string street, int house, int flat)
+		{
+			this->city = city;
+			this->street = street;
+			this->house = house;
+			this->flat = flat;
+		}
+
+		Adress() {}
+
+		std::string adressInLine()
+		{
+			return city + ", " + street + ", " + std::to_string(house) + ", " + std::to_string(flat);
+		}
+	};
+		
+	int quantityAdress;
+	std::vector <Adress> adresses;
+
+	bool readF(std::string fName)
+	{
+		std::ifstream file(fName);
+		std::string city;
+		std::string street;
+		int house;
+		int flat;
+
+		if (!file.is_open()) {
+			std::cout << "Текстовый файл не найден" << std::endl;
+			return false;
+		}
+
+		file >> quantityAdress;
+		adresses.reserve(quantityAdress);
+
+		for (int i = 0; i < quantityAdress; ++i)
+		{
+			file >> city;
+			file >> street;
+			file >> house;
+			file >> flat;
+
+			adresses.push_back(Adress(city, street, house, flat));
+		}
+
+		file.close();
+		return true;
+	};
+
+	void saveF(std::string fName)
+	{
+		std::ofstream file(fName);
+		file << quantityAdress << std::endl;
+		for (int i = 0; i < quantityAdress; i++)
+		{
+			file << adresses[i].adressInLine() << std::endl;
+		}
+		file.close();
+	};
+};
+
+int main()
+{
+	setlocale(LC_ALL, "ru-RU");
+	system("chcp 1251");
+	
+	Base base;
+
+	if (!base.readF("in.txt"))
+	{
+		std::cout << "Исходный файл не прочитан";
+		return ;
+	} 
+	else
+	{
+		base.saveF("out.txt");
+		std::cout << "Успешное выполнение программы";
+		return 0;
+	}
+}
---
+++ cppm-hw-04-02/cppm-hw-04-02.cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+class Base
+{
+public:
+	class Adress
+	{
+	public:
+		std::string city;
+		std::string street;
+		int house;
+		int flat;
+
+		Adress(std::string city, std::string street, int house, int flat)
+		{
+			this->city = city;
+			this->street = street;
+			this->house = house;
+			this->flat = flat;
+		}
+
+		Adress() {}
+
+		std::string adressInLine()
+		{
+			return city + ", " + street + ", " + std::to_string(house) + ", " + std::to_string(flat);
+		}
+	};
+
+	int quantityAdress;
+	std::vector <Adress> adresses;
+
+	bool readF(std::string fName)
+	{
+		std::ifstream file(fName);
+		std::string city;
+		std::string street;
+		int house;
+		int flat;
+
+		if (!file.is_open()) {
+			return false;
+		}
+
+		file >> quantityAdress;
+		adresses.reserve(quantityAdress);
+
+		for (int i = 0; i < quantityAdress; ++i)
+		{
+			file >> city;
+			file >> street;
+			file >> house;
+			file >> flat;
+
+			adresses.push_back(Adress(city, street, house, flat));
+		}
+
+		file.close();
+		return true;
+	};
+
+	void saveF(std::string fName)
+	{
+		std::ofstream file(fName);
+		file << quantityAdress << std::endl;
+		for (int i = 0; i < quantityAdress; i++)
+		{
+			file << adresses[i].adressInLine() << std::endl;
+		}
+		file.close();
+	};
+
+	void sort()
+	{
+		for (int i = 0; i < quantityAdress; ++i)
+		{
+			for (int j = 0; j < (quantityAdress - i - 1); ++j)
+			{
+				if (adresses[j].city > adresses[j + 1].city)
+				{
+					Adress time = adresses[j];
+					adresses[j] = adresses[j + 1];
+					adresses[j + 1] = time;
+				}
+			}
+		}
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "ru-RU");
+	system("chcp 1251");
+
+	Base base;
+
+	if (!base.readF("in.txt"))
+	{
+		std::cout << "Исходный файл не прочитан";
+		return 1;
+	}
+
+	base.sort();
+	base.saveF("out.txt");
+	std::cout << "Успешное выполнение программы";
+	return 0;
+}
